bdc0498c55e66031b78758b31b2b738b05e3d0bd
Daniel Sedoff <danielsedoff@gmail.com>
2024-01-13 23:57:57 +0300

    add dark mode toggle checkbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
 import '../css/style.css'
+
+const toggleDark = document.querySelectorAll('#toggleDark')[0]
+toggleDark.addEventListener('click', _ => document.documentElement.classList.toggle('dark'))
