baebd1b7735c62d6ef3fdcb28dfb699754b36cd1
Daniel Sedoff <danielsedoff@gmail.com>
2024-01-14 22:20:02 +0300

    save dark mode setting and checkbox.checked to localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
 import '../css/style.css'
 
 const toggleDark = document.querySelectorAll('#toggleDark')[0]
-toggleDark.addEventListener('click', _ => document.documentElement.classList.toggle('dark'))
+const doc = document.documentElement
+
+if (localStorage.getItem('mode') === 'dark') {
+  doc.classList.add('dark')
+  toggleDark.checked = true
+}
+
+toggleDark.addEventListener('click', _ => {
+  doc.classList.toggle('dark')
+  if (doc.classList.contains('dark')) {
+    localStorage.setItem('mode', 'dark')
+  } else {
+    localStorage.setItem('mode', 'light')
+  }
+})
+
+localStorage.setItem('onlyStringsAreSaved', JSON.stringify({key:'value'})) // F12 -> Application -> Local Storage
+const objectFromLocalStorage = JSON.parse(localStorage.getItem('onlyStringsAreSaved'))
+
