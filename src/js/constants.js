bdea6a18a4bb934a295259559dc900e39ad4d963
Daniel Sedoff <danielsedoff@gmail.com>
2024-01-20 18:05:24 +0300

    add keyboard basics

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,1 +1,2 @@
 export const WORDS = ['hello', 'everyone', 'beer']
+export const KEYBOARD_LETTERS = [...Array(26)].map((e, i) => (i + 10).toString(36).toUpperCase())
--- src/js/game.js
+++ src/js/game.js
@@ -1,28 +1,46 @@
-import { WORDS } from "./constants"
+import { WORDS, KEYBOARD_LETTERS } from "./constants"
 
 const gameDiv = document.querySelectorAll('#game')[0]
 
+// Produce a pseudorandom integer in the given range
 const randomInt = (min, max) => {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
 
-const getPlaceholders = str => {
+// Create the placeholders, these will be HTML elements.
+const createPlaceholders = str => {
     let placeholders = Array.from('_'.repeat(str.length)).reduce((acc, curr, i) => {
         return acc + `<h1 id="letter_${i}" class="letter">_</h1>`
     }, '')
-
     return `<div id="placeholders" class="placeholder-wrapper">${placeholders}</div>`
 }
 
+const createKeyboard = () => {
+    let keyboard = document.createElement('div')
+    keyboard.classList.add('keyboard')
+    keyboard.id = 'keyboard'
+
+    const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, curr) => {
+        let button = `<button class="button-primary" id="${curr}">${curr}</button>`
+        return acc + button
+    }, '')
+
+    keyboard.innerHTML = keyboardHTML
+    return keyboard
+}
+
+// Assemble the whole Game div element from components.
 const createGameDiv = () => {
     const randomIndex = randomInt(0, WORDS.length - 1)
     const wordToGuess = WORDS[randomIndex]
     sessionStorage.setItem('wordToGuess', wordToGuess)
-    return `<h1>${getPlaceholders(wordToGuess)}</h1>`
+    return (`<h1>${createPlaceholders(wordToGuess)}</h1>`)
 }
 
-
+// This function is called on click.
 export const startGame = _ => {
     gameDiv.innerHTML = createGameDiv()
+    // gameDiv.innerHTML += createKeyboad | yields [object HTMLDivElement]
+    gameDiv.appendChild(createKeyboard())
 }
