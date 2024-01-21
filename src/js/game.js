429f62b257e6108ba1693417d2ea0bb6eea4bfd8
Daniel Sedoff <danielsedoff@gmail.com>
2024-01-21 13:27:19 +0300

    reformat with prettier

--- src/js/game.js
+++ src/js/game.js
@@ -1,22 +1,24 @@
-import { WORDS, KEYBOARD_LETTERS } from "./constants"
+import { WORDS, KEYBOARD_LETTERS } from './constants'
 
 const gameDiv = document.querySelectorAll('#game')[0]
 const LogoHeader = document.getElementById('logo')
 
 // Produce a pseudorandom integer in the given range
 const randomInt = (min, max) => {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
+    let rand = min + Math.random() * (max + 1 - min)
+    return Math.floor(rand)
+}
 
 // Create the placeholders, these will be HTML elements.
 const createPlaceholders = str => {
-    let placeholders = Array.from('_'.repeat(str.length)).reduce((acc, curr, i) => {
-        return acc + `<h1 id="letter_${i}" class="letter">_</h1>`
-    }, '')
+    let placeholders = Array.from('_'.repeat(str.length)).reduce(
+        (acc, curr, i) => {
+            return acc + `<h1 id="letter_${i}" class="letter">_</h1>`
+        },
+        ''
+    )
     return `<div id="placeholders" class="placeholder-wrapper">${placeholders}</div>`
 }
-
 
 const createKeyboard = () => {
     let keyboard = document.createElement('div')
@@ -37,10 +39,10 @@ const createGameDiv = () => {
     const randomIndex = randomInt(0, WORDS.length - 1)
     const wordToGuess = WORDS[randomIndex]
     sessionStorage.setItem('wordToGuess', wordToGuess)
-    return (`<h1>${createPlaceholders(wordToGuess)}</h1>`)
+    return `<h1>${createPlaceholders(wordToGuess)}</h1>`
 }
 
-const createHangmanImg = (hg) => {
+const createHangmanImg = hg => {
     const hangmanImg = document.createElement('img')
     hangmanImg.src = `images/hg-${hg}.png`
     hangmanImg.alt = `hangman image ${hg}.png`
@@ -50,7 +52,7 @@ const createHangmanImg = (hg) => {
     return hangmanImg
 }
 
-const createAttemptDiv = (num) => {
+const createAttemptDiv = num => {
     return `<p id="attempts" class="mt-2">ATTEMPTS LEFT: <span id="attempts-left" class="font-medium text-red-600">${num}</span></p>`
 }
 
@@ -64,11 +66,10 @@ export const startGame = _ => {
 
     // gameDiv.innerHTML += createKeyboad | yields [object HTMLDivElement]
     let keyb = createKeyboard()
-    keyb.addEventListener('click', (event) => {
-        console.log(event.target.innerHTML);
+    keyb.addEventListener('click', event => {
+        console.log(event.target.innerHTML)
     })
     gameDiv.appendChild(keyb)
-
 
     let hangmanImg = createHangmanImg(0)
     gameDiv.prepend(hangmanImg)
