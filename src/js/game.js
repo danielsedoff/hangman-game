ea01b1b767866443c208c0645dc50bea116c2795
Daniel Sedoff <danielsedoff@gmail.com>
2024-01-21 16:48:52 +0300

    add endgame and restart functionality

--- src/js/game.js
+++ src/js/game.js
@@ -61,10 +61,17 @@ const doBadGuess = () => {
     const pic = document.getElementById('hangman-img')
     let imgNum = 10 - attemptsLeft
     if (imgNum > 9){
-        let keyb = document.getElementById('keyboard')
-        keyb.style += '; display: none; '
+        endGame('fail')
     }
     pic.src = `images/hg-${imgNum}.png`
+}
+
+const checkWin = word => {
+    let result = true
+    word.split('').forEach((letter, i) => {
+        if (document.getElementById(`letter_${i}`).innerText.toLowerCase() !== letter.toLowerCase()) result = false
+    })
+    return result
 }
 
 const doGoodGuess = (word, letter) => {
@@ -74,6 +81,9 @@ const doGoodGuess = (word, letter) => {
             document.getElementById(`letter_${i}`).innerText = letter
         }
     });
+
+    if(checkWin(word)) endGame('win')    
+
 }
 
 const checkLetter = keyElement => {
@@ -111,4 +121,32 @@ export const startGame = _ => {
 
     let hangmanImg = createHangmanImg(0)
     gameDiv.prepend(hangmanImg)
+}
+
+const endGame = status => {
+
+    document.getElementById('keyboard').style.display = 'none'
+    document.getElementById('placeholders').remove()
+
+    let resultHeader = document.createElement('h2')
+    resultHeader.classList.add("result-header")
+
+
+    if (status === 'win'){
+        resultHeader.innerText = "You win!"
+        resultHeader.classList.add('win')
+        document.getElementById('hangman-img').src = 'images/hg-win.png'
+    } else {
+        resultHeader.innerText = "You lose!"
+        resultHeader.classList.add('lose')
+    }
+
+    let restartButton = document.createElement('button')
+    restartButton.innerText = 'PLAY AGAIN'
+    restartButton.classList.add('start-button')
+    restartButton.classList.add('mt-10')
+    document.getElementById('game').appendChild(resultHeader)
+    document.getElementById('game').appendChild(restartButton)
+
+    restartButton.addEventListener('click', startGame)
 }
